963bb664bdab3ff7c793eee3c64d12af24b12a9c
nastymorbol <nastymorbol@gmail.com>
2022-05-13 08:52:50 +0200

    2022-05-13 BACnetDevice UseStaticBinding is now available threw Set

--- copy_pm.sh
+++ copy_pm.sh
@@ -12,8 +12,9 @@ done
 #array=( "03_MbusNetwork.pm" "00_OPENems.pm" "01_FupMacro.pm" "00_BACnetNetwork.pm" "01_BACnetDevice.pm" "02_BACnetDatapoint.pm" "00_OPENgate.pm" "00_OPENweb.pm" "99_myUtils.pm")
 
 user=root
-ip=192.168.123.59
-#ip=172.20.47.200
+#ip=192.168.123.59
+ip=172.20.47.200
+#ip=172.20.47.60
 #ip=172.20.37.51
 #id="~/.ssh/test_rsa"
 id="~/.ssh/id_rsa"
